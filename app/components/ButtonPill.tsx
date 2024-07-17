7e1adebe217f90e57abc635afe2045bbe7516b41
David Kacurik <david.kacurik@bluelemons.sk>
2024-07-17 11:43:16 +0200

    ADD - scrolling

--- app/components/ButtonPill.tsx
+++ app/components/ButtonPill.tsx
@@ -5,18 +5,22 @@ interface ButtonPillProps {
   children: ReactNode
   state: string
   onClick: MouseEventHandler<HTMLButtonElement>
+  className?: string
 }
 
 const ButtonPill = ({
   children,
   state,
   onClick,
+  className,
 }: ButtonPillProps): JSX.Element => {
   return (
     <Button
       onClick={onClick}
       className={
-        'px-[2rem] py-[1rem] text-m font-semibold rounded-full hover:bg-primary hover:text-black min-w-[120px]' +
+        `${
+          className && className
+        } px-[2rem] py-[1rem] text-m font-semibold rounded-full hover:bg-primary hover:text-black min-w-[120px]` +
         (state === 'active'
           ? ' bg-primary text-black'
           : state === 'used'
--- app/components/DaySelector.tsx
+++ app/components/DaySelector.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { MouseEvent } from 'react'
+import React, { MouseEvent, useEffect, useRef } from 'react'
 import Stack from '@mui/material/Stack'
 import ButtonPill from './ButtonPill'
 import { Context } from '../context'
@@ -16,6 +15,7 @@ const DaySelector = ({
   selectorRef,
 }: DaySelectorProps) => {
   const [confirmed, setConfirmed] = React.useState<string[]>([])
+  const stackRef = useRef<HTMLDivElement>(null) // Ref for the Stack component
 
   const days = ['1', '2', '3', '4', '5']
   const context = React.useContext(Context)
@@ -26,20 +26,32 @@ const DaySelector = ({
 
   const { confirmedMeals } = context
 
-  React.useEffect(() => {
+  useEffect(() => {
     confirmedMeals.meals.forEach((meal) => {
       setConfirmed((prev) => [...prev, `btn-${meal.day}`])
     })
   }, [confirmedMeals])
 
+  // Scroll to the active day's ButtonPill element
+  useEffect(() => {
+    if (stackRef.current) {
+      const buttonPill = stackRef.current.querySelector(`.btn-${activeDay}`)
+      if (buttonPill) {
+        buttonPill.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+      }
+    }
+  }, [activeDay])
+
   const handleClick =
     (day: string) => (event: MouseEvent<HTMLButtonElement>) => {
       activeDayHandler(day)
     }
+
   return (
     <div ref={selectorRef}>
       <h2 className="mt-[2.5rem] text-l">Naplánujte si jednotlivé dni</h2>
       <Stack
+        ref={stackRef}
         direction={{ sm: 'row', xs: 'row' }}
         spacing={2}
         className="mt-[1.5rem] overflow-x-scroll no-scrollbar"
@@ -55,6 +67,7 @@ const DaySelector = ({
             }
             key={day}
             onClick={handleClick(day)}
+            className={`btn-${day}`} // Add class for each ButtonPill element
           >
             {day}. DEŇ
           </ButtonPill>
